d1c1e24a6ec7451116753808dbb499a9054d5d33
朱邓全 <zhudengquan@nyato.com>
2017-08-04 18:07:18 +0800

    change image

--- PageScoll/ViewController.swift
+++ PageScoll/ViewController.swift
@@ -33,8 +33,8 @@ class ViewController: UIViewController {
     
     private func configureContainerViewController() {
         headerViewController = PageContainerController()
-//        headerViewController.imagesName = ["login0", "login1", "login2"]
-//        headerViewController.pagesTitle = ["Page 0", "Page 1", "Page 2"]
+        headerViewController.imagesName = ["0", "1", "2"]
+        headerViewController.pagesTitle = ["Page 0", "Page 1", "Page 2"]
         
         headerViewController.delegate = self
         
